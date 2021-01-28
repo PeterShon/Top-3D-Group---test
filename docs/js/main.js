5ef0ad2603fadbec48f92e539bd4b8cca657b216
PeterShon <petershonpublic@gmail.com>
2021-01-28 14:05:03 +0300

    2021-01-28__header adopt

--- docs/js/main.js
+++ docs/js/main.js
@@ -3000,19 +3000,42 @@ __webpack_require__.r(__webpack_exports__);
 /* burger */
 function burger() {
   var burger = document.querySelector('.burger');
+  var topLine = document.querySelector('.header__top-line');
+  var navBtn = document.querySelector('.burger__title_nav');
+  var categoryBtn = document.querySelector('.burger__title_category');
+  var nav = document.querySelector('.burger__nav');
+  var category = document.querySelector('.burger__category');
   var burgerBtn = burger.querySelector('.burger__button');
   var body = document.querySelector('body');
   burgerBtn.addEventListener('click', setStatus);
+  navBtn.addEventListener('click', setStatusNav);
+  categoryBtn.addEventListener('click', setStatusNav);
 
   function setStatus() {
     if (burger.classList.contains('burger_active')) {
+      topLine.classList.remove('header__top-line_active');
       burger.classList.remove('burger_active');
       burger.classList.add('burger_unactive');
       body.classList.remove('lock');
     } else {
+      topLine.classList.add('header__top-line_active');
       burger.classList.add('burger_active');
       burger.classList.remove('burger_unactive');
       body.classList.add('lock');
+    }
+  }
+
+  function setStatusNav() {
+    if (nav.classList.contains('burger__nav_active')) {
+      navBtn.classList.remove('js_active');
+      categoryBtn.classList.add('js_active');
+      nav.classList.remove('burger__nav_active');
+      category.classList.add('burger__category_active');
+    } else {
+      navBtn.classList.add('js_active');
+      categoryBtn.classList.remove('js_active');
+      nav.classList.add('burger__nav_active');
+      category.classList.remove('burger__category_active');
     }
   }
 }
--- src/js/burger.js
+++ src/js/burger.js
@@ -1,20 +1,43 @@
 /* burger */
 export function burger() {
   let burger = document.querySelector('.burger')
+  let topLine = document.querySelector('.header__top-line')
+  let navBtn = document.querySelector('.burger__title_nav')
+  let categoryBtn = document.querySelector('.burger__title_category')
+  let nav = document.querySelector('.burger__nav')
+  let category = document.querySelector('.burger__category')
   let burgerBtn = burger.querySelector('.burger__button')
   let body = document.querySelector('body')
 
   burgerBtn.addEventListener('click', setStatus)
+  navBtn.addEventListener('click', setStatusNav)
+  categoryBtn.addEventListener('click', setStatusNav)
 
   function setStatus() {
     if (burger.classList.contains('burger_active')) {
+      topLine.classList.remove('header__top-line_active')
       burger.classList.remove('burger_active')
       burger.classList.add('burger_unactive')
       body.classList.remove('lock')
     } else {
+      topLine.classList.add('header__top-line_active')
       burger.classList.add('burger_active')
       burger.classList.remove('burger_unactive')
       body.classList.add('lock')
+    }
+  }
+
+  function setStatusNav() {
+    if (nav.classList.contains('burger__nav_active')) {
+      navBtn.classList.remove('js_active')
+      categoryBtn.classList.add('js_active')
+      nav.classList.remove('burger__nav_active')
+      category.classList.add('burger__category_active')
+    } else {
+      navBtn.classList.add('js_active')
+      categoryBtn.classList.remove('js_active')
+      nav.classList.add('burger__nav_active')
+      category.classList.remove('burger__category_active')
     }
   }
 
